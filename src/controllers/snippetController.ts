3c9783b9f5b00f506e9ab022a4d3c975960986c7
Domien Grysolle <domien-grysolle@hotmail.com>
2025-03-12 11:20:54 +0100

    added getSnippets

--- src/controllers/snippetController.ts
+++ src/controllers/snippetController.ts
@@ -22,3 +22,20 @@ export const createSnippet = async (req: Request, res: Response) => {
     }
   }
 };
+
+export const getSnippets = async (req: Request, res: Response) => {
+  try {
+    const snippets = await Snippet.find();
+    snippets.forEach(
+      (snippet) =>
+        (snippet.code = Buffer.from(snippet.code, "base64").toString("utf-8"))
+    );
+    res.json(snippets);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
+    } else {
+      res.status(500).json({ message: "Something went wrong" });
+    }
+  }
+};
